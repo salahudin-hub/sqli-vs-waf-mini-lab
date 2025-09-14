9912702849bdcf93118dead847e8d1356bbb9f01
salahudin-hub <pippin_campus_0u@icloud.com>
2025-09-14 18:02:06 +0300

    Add DVWA configuration file

---
+++ conf/dvwa/config.inc.php
@@ -0,0 +1,29 @@
+<?php
+# Database management system to use
+$DBMS = 'MySQL';
+# Database type
+$DB_TYPE = 'mysqli';
+# Database address
+$db_server = 'db';
+# Database port
+$db_port = '3306';
+# Database name
+$db_database = 'dvwa';
+# Database user
+$db_user = 'dvwa';
+# Database password
+$db_password = 'p@ssw0rd';
+# Database table prefix
+$db_prefix = '';
+# Database error mode
+$db_error_mode = PDO::ERRMODE_SILENT;
+
+# Default security level
+$default_security_level = 'low';
+
+# Default PHP error reporting
+$error_reporting = E_ALL;
+
+# Default locale
+$locale = 'en_GB';
+?>
